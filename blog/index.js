3c9116bc89040da9bdf262ff2439b8151ac157aa
Yoannes <yoannes@gmail.com>
2020-06-23 12:44:31 +0900

    blog

---
+++ blog/index.js
@@ -0,0 +1,18 @@
+// Flex
+// https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+
+const App = {
+  init: function () {
+    console.log("Start of our super blog");
+  },
+
+  events: {},
+
+  components: {
+    createComponents: function () {},
+    menuContainer: null,
+    bodyContainer: null,
+  },
+};
+
+App.init();
