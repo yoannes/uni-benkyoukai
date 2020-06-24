02659c0ae846b665fafb28aa5c375ebc56a2aeb9
hyumanase <hyumayamaguchi@gmail.com>
2020-06-24 22:52:50 +0900

    Add formContainer and formContainerAddBtn

--- blog/index.js
+++ blog/index.js
@@ -17,23 +17,43 @@ const App = {
     this.components.container.style.width = "1000px";
     this.components.container.style.height = "600px";
 
+
+    this.components.formContainer.style.position = "absolute";
+    this.components.formContainer.style.top = "0";
+    this.components.formContainer.style.left = "0";
+    this.components.formContainer.style.right = "0";
+    this.components.formContainer.style.bottom = "0";
+    this.components.formContainer.style.margin = "auto";
+    this.components.formContainer.style.border = "solid";
+    this.components.formContainer.style.color = "purple";
+    this.components.formContainer.style.width = "400px";
+    this.components.formContainer.style.height = "150px";
+
+    this.components.formContainerAddBtn.style.marginTop = "10px";
+    this.components.formContainerAddBtn.style.border = "solid";
+    this.components.formContainerAddBtn.style.color = "purple";
+    this.components.formContainerAddBtn.style.width = "100px";
+    this.components.formContainerAddBtn.style.height = "30px";
+
     this.components.menuContainer.style.border = "solid";
     this.components.menuContainer.style.color = "black";
     this.components.menuContainer.style.width = "200px";
     this.components.menuContainer.style.height = "600px";
-    this.components.menuContainer.style.overflow = "scroll";
+    this.components.menuContainer.style.overflowY = "auto";
+    this.components.menuContainer.style.overflowX = "hidden";
     
     this.components.bodyContainer.style.border = "solid";
     this.components.bodyContainer.style.color = "red";
     this.components.bodyContainer.style.width = "800px";
     this.components.bodyContainer.style.height = "600px";
     
+    
     for (let i = 0; i < data.length; i++) {
       const item = data[i];
       this.posts.create(item)
     }
-  },
 
+  },
 
   posts: {
     selectedPost: null,
@@ -46,6 +66,13 @@ const App = {
         const el = document.createElement("div");
         el.id = item.id
         el.innerHTML = item.title
+        
+        el.style.border = "solid";
+        el.style.borderColor = "green";
+        el.style.width = "200px";
+        el.style.height = "50px";
+        el.style.overflow = "hidden";
+        el.style.cursor = "pointer";
         
         // Create element click listener
         el.onclick = function(){
@@ -60,6 +87,7 @@ const App = {
 
         // Append element to container
         App.components.menuContainer.appendChild(el);
+
       }
     },
 
@@ -80,24 +108,33 @@ const App = {
       this.container = document.createElement("div");
       this.app.appendChild(this.container);
 
+      this.formContainer = document.createElement("div");
+      this.app.appendChild(this.formContainer);
+
+      this.formContainerAddBtn = document.createElement("div");
+      this.app.appendChild(this.formContainerAddBtn);
+
+
       this.menuContainer = document.createElement("div");
       this.container.appendChild(this.menuContainer);
       
       this.bodyContainer = document.createElement("div");
       this.container.appendChild(this.bodyContainer);
 
-      this.bodyContainer = document.createElement("div");
-      this.container.appendChild(this.bodyContainer);
     },
+
     container: null,
+    formContainer: null,
+    formContainerAddBtn: null,
+
     menuContainer: null,
     bodyContainer: null,
-    menuLists: {
-      
+    menuLists: {    
     }
     
+
   },
-  
+
 };
 
 App.init();
